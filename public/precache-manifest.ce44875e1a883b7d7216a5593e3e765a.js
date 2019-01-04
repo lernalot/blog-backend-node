bf2c646b0d8f10b2ff336fd72891150cbcdd3e75
Aoyou <karlao@xwfintech.com>
2019-01-04 10:05:13 +0800

    dist

---
+++ public/precache-manifest.ce44875e1a883b7d7216a5593e3e765a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b70c8d2047b9db28660c11749bd843aa",
+    "url": "/static/media/index.b70c8d20.less"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02d7ddf64f40dbdbd4f8",
+    "url": "/static/js/main.02d7ddf6.chunk.js"
+  },
+  {
+    "revision": "ebe40dfabd7f791e4ba6",
+    "url": "/static/js/1.ebe40dfa.chunk.js"
+  },
+  {
+    "revision": "02d7ddf64f40dbdbd4f8",
+    "url": "/static/css/main.160b8fab.chunk.css"
+  },
+  {
+    "revision": "f54f9fd5feaa2e6fd081776762aad380",
+    "url": "/index.html"
+  }
+];
